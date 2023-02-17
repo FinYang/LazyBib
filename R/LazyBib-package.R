5b72b6840d751c9ded97f96f82fcbbb83f0c0ab9
Fin Yang <yangyangzhuoran@gmail.com>
2023-02-17 14:54:34 +1100

    Make into working package and deploy

---
+++ R/LazyBib-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom magrittr %>%
+#' @importFrom stats setNames
+#' @importFrom stringr str_split
+## usethis namespace: end
+NULL
